7b1097992ea16d496a92f438a507443f6ea8ecc4
Richard Henry Rozario <richardrozario.rr@gmail.com>
2024-02-14 15:59:05 +0530

    using calendly package

--- src/components/Booking/Booking.jsx
+++ src/components/Booking/Booking.jsx
@@ -9,37 +9,14 @@ import {
   textVariant,
   textVariant2,
 } from "../../utils/motion";
+import { InlineWidget } from "react-calendly";
 const Booking = () => {
   return (
-    <motion.section
-      variants={staggerChildren}
-      initial="hidden"
-      whileInView="show"
-      viewport={{ once: false, amount: 0.25 }}
-      section
-      className={`paddings ${css.wrapper}`}
-    >
-      <a className="anchor" id="booking"></a>
-
-      <motion.div
-        variants={footerVariants}
-        className={`yPaddings innerWidth ${css.container}`}
-      >
-        <div className={`flexCenter ${css.heading}`}>
-          <span className="primaryText">Book an appointment</span>
-        </div>
-
-        <div
-          class="calendly-inline-widget"
-          data-url="https://calendly.com/kapestar-founder/30min"
-          style={{
-            width: "100%",
-            height: "700px",
-          }}
-        ></div>
-      </motion.div>
-    </motion.section>
+    <section id="booking">
+      <InlineWidget url="https://calendly.com/kapestar-founder/30min" />
+    </section>
   );
 };
 
+// data-url="https://calendly.com/kapestar-founder/30min"
 export default Booking;
